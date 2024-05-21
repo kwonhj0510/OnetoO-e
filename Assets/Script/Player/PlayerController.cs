5e6cf9aed6e9e7da70d1f9e0f4d6dd39102d5807
kwonhj0510 <sdh230201@sdh.hs.kr>
2024-05-22 00:04:38 +0900

    PlayerController
    CameraRotate, PlayerMoveMent

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Input KeyCodes")]
+    [SerializeField]
+    private KeyCode keyCodeRun = KeyCode.LeftShift;   // 달리기 키
+
+    private RotateToMouse   rotateToMouse;              // 마우스 이동으로 카메라 회전
+    private PlayerMovement  movement;                   // 키보드 입력으로 플레이어 이동, 점프
+    private Status          status;                     // 이동속도 등의 플레이어 정보
+           
+    void Awake()
+    {
+        // 마우스 커서를 보이지 않게하고 현재 위치에 고정시킨다.
+        Cursor.visible      = false;
+        Cursor.lockState    = CursorLockMode.Locked;
+                
+        rotateToMouse       = GetComponent<RotateToMouse>();
+        movement            = GetComponent<PlayerMovement>();
+        status              = GetComponent<Status>();
+    }
+
+    void Update()
+    {
+        UpdateRotate();
+        UpdateMove();
+    }
+
+    private void UpdateRotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        rotateToMouse.UpdateRotate(mouseX, mouseY);
+
+    }
+
+    private void UpdateMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+       // 이동 중일때 (걷기, 뛰기)
+       if( x != 0 || z != 0 )
+        {
+            bool isRun = false;
+
+            // 옆이나 뒤로 이동할 때는 달릴 수 없다.
+            if (z > 0) isRun = Input.GetKey(keyCodeRun);
+
+            movement.MoveSpeed = isRun == true ? status.RunSpeed : status.WalkSpeed;
+        }
+        movement.MoveTo(new Vector3(x, 0, z));
+    }
+}

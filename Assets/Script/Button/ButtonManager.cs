0aed05e70dffd69a72b78efe06c57b5eb1455e16
kwonhj0510 <sdh230201@sdh.hs.kr>
2024-06-10 00:03:06 +0900

    Close Button

--- Assets/Script/Button/ButtonManager.cs
+++ Assets/Script/Button/ButtonManager.cs
@@ -32,30 +32,37 @@ public class ButtonManager : MonoBehaviour
                 case "QUIT":
                     button.onClick.AddListener(QuitGame);
                     break;
+                case "CLOSE":
+                    button.onClick.AddListener(CloseWindow);
+                    break;
                 default:
                     break;
             }
         }
     }
 
-    private void StartGame()
+    private void StartGame()    // Start 버튼을 누르면 인게임 화면으로 넘어간다.
     {
         SceneManager.LoadScene("InGame");
     }
-    private void OpenTutorial()
+    private void OpenTutorial() //Tutorial 버튼을 누르면 튜토리얼 창으로 넘어간다.
     {
         SceneManager.LoadScene("Tutorial");
     }
-    private void OpenSettings()
+    private void OpenSettings() // Settings 버튼을 누르면 설정 창으로 넘어간다.
     {
         SceneManager.LoadScene("Settings");
     }
-    private void ShowCredits()
+    private void ShowCredits()  // Credits 버튼을 누르면 크레딧 창으로 넘어간다.
     {
         SceneManager.LoadScene("Credits");
     }
-    private void QuitGame()
+    private void QuitGame()     // Quit 버튼을 누르면 게임이 종료된다.
     {
         Application.Quit();
+    }
+    private void CloseWindow()  // Close 버튼을 ( X ) 누르면 창이 닫히고 메인 화면으로 돌아간다.
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }

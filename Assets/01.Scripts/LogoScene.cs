d22c0ca6b9870766ad2ffd411a7179378ea70996
kwonhj0510 <sdh230201@sdh.hs.kr>
2024-07-12 09:12:44 +0900

    Add LogoScence

---
+++ Assets/01.Scripts/LogoScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoScene : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Co_TitleScene());
+    }
+
+    private  IEnumerator Co_TitleScene()
+    {
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("00.MainMenu");
+    }
+}
